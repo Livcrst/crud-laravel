40157de546461ce4f0093d5d8dc9aa5eadd9400b
Lia <livcristinna@gmail.com>
2025-10-27 22:24:49 -0300

    create controllers

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,47 +2,66 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Products;
+use App\Models\Product;
 use Illuminate\Http\Request;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Response;
+use Illuminate\View\View;
 
-class ProductsController extends Controller
-{
+class ProductController extends Controller
+{ 
+
+    // Aqui fica a integração entre servidor e telas que estamos desenvolvendo.
     /**
      * Display a listing of the resource.
      */
     public function index()
+    // Para cada objeto ele cria um novo padrão. a index extende as caracteristicas de view.
     {
-        //
+        $products = Product::latest()->paginate(5);
+        // return view('products.index', compact('products'));
+        return view('products.index', compact('products'))
+            ->with('i', (request()->input('page', 1) - 1) * 5);
     }
 
     /**
      * Show the form for creating a new resource.
      */
-    public function create()
+    public function create(): View
     {
-        //
+        return view('products.create');
+        // é uma telinha que aparece para o user colocar os dados
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(Request $request):RedirectResponse
     {
-        //
+        $request->validate([
+            'name' => 'required',
+            'description' => 'required',
+            'price' => 'required|numeric',
+        ]);
+
+        Product::create($request->all());
+
+        return redirect()->route('products.index')
+            ->with('success', 'Product created successfully.');
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(Products $products)
+    public function show(Product $products)
     {
-        //
+        return view('products.show', compact('products'));
     }
-
+ 
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(Products $products)
+    public function edit(Product $products)
     {
         //
     }
@@ -50,7 +69,7 @@ class ProductsController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, Products $products)
+    public function update(Request $request, Product $products)
     {
         //
     }
@@ -58,7 +77,7 @@ class ProductsController extends Controller
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(Products $products)
+    public function destroy(Product $products)
     {
         //
     }
--- app/Models/Product.php
+++ app/Models/Product.php
@@ -5,7 +5,15 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Products extends Model
+class Product extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        // Esse fillable define os campos que podem ser preenchidos em massa. É uma variavel de ambiente. 
+        // Coisas que podem ser incluidas pelo usuario de forma mais manual.
+        'name',
+        'description',
+        'price',
+    ];
 }
